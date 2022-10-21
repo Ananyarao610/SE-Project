1c383b736dec4481c7c991980d60450c1e490975
Ananya Rao <ananyarao610@gmail.com>
2022-10-21 20:18:49 +0530

    finally works

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 # FLASK_APP=app.py FLASK_ENV=development flask run
 from turtle import heading
 from pure_eval import group_expressions
-import text_preprocessing 
+
 from flask import Flask
 from flask import Flask, flash, redirect, render_template, request, session, jsonify
 import json
@@ -13,72 +13,10 @@ app = Flask(__name__)
 #   return render_template("h.html")  
 
 
-@app.route("/experiment")
+@app.route("/")
 def experiment(): 
-  objects = []    
-  new=[]
-  check_box=[] 
-  f = open("static/text/titration.txt", "r")
-  para= f.read()
-  f = open("static/text/groups_sa.txt", "r")
-  groups = f.read()
-  qwe=text_preprocessing.main(para)   
-  apparatus = []
-  for i in qwe:    
-    for q in i:
-        print("this is individual objects in each sent",q)
-        objects.append(json.dumps(q))
-    new.append(objects)
-    check_box.append(objects)
-    objects=[]
-    heading="Titration"
-  return render_template("experimentPage.html", objs = new, para=para, heading=heading, groups=groups)     
+  return render_template("index2.html")     
 
-
-@app.route("/experiment1")
-def experiment1():
-  objects = []    
-  new=[]
-  check_box=[] 
-  f = open("static/text/basic_radical.txt", "r")
-  para= f.read()
-  f = open("static/text/groups_br.txt", "r")
-  groups = f.read()
-  qwe=text_preprocessing.main(para)   
-  apparatus = []
-  for i in qwe:   
-    for q in i:
-        print("this is individual objects in each sent",q)
-        objects.append(json.dumps(q))
-    new.append(objects)
-    check_box.append(objects)
-    objects=[]
-    heading= "Basic Radical Detection"
-  return render_template("experimentPage.html", objs = new, para=para, groups=groups, heading = heading)  
-
-  
-
-@app.route("/experiment2")
-def experiment2():
-  objects = []   
-  new=[]
-  check_box=[] 
-  f = open("static/text/salt_analysis.txt", "r")
-  para= f.read()
-  f = open("static/text/groups_sa.txt", "r")
-  groups = f.read()
-  qwe=text_preprocessing.main(para)  
-  apparatus = []
-  for i in qwe:   
-    for q in i:
-        print("this is individual objects in each sent",q)
-        objects.append(json.dumps(q))
-    new.append(objects)
-    check_box.append(objects)
-    objects=[]
-    heading="Salt Analysis"
-    
-  return render_template("experimentPage.html", objs = new, para=para, groups=groups, heading=heading)  
 
 if __name__=='__main__':
   app.run(debug=True,port=5000) 
--- static/js/script.js
+++ static/js/script.js
@@ -1,11 +1,19 @@
-var sentence_index = 0, numOfSentences, sentences=[], o, paragraph, toDisplaySentences,toDisplayGroups, wordToSpeakIndex = 0;
-const synth = window.speechSynthesis;
-var voices = []
-document.addEventListener("DOMContentLoaded", function (){
-  synth.cancel();
-  preprocess();  
-  decodeSentence();
-  });
+// document.addEventListener("DOMContentLoaded", function(){
+//     VANTA.NET({
+//         el: "#wrapper",
+//         mouseControls: true,
+//         touchControls: true,
+//         gyroControls: false,
+//         minHeight: 200.00,
+//         minWidth: 200.00,
+//         scale: 1.00,
+//         scaleMobile: 1.00,
+//         color: 0x8919a0,
+//         backgroundColor: 0x0,
+//         maxDistance: 22.00
+//     })
+// });
 
-
-
+document.addEventListener("DOMContentLoaded", () => {
+    
+});
